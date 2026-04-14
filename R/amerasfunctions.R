8e60237c3ca0acbd9d3a9ff76675bf997c1aff98
Sander Roberti <github@sanderroberti.nl>
2026-04-14 11:28:28 -0400

    Moved nimble to imports from depends

--- R/amerasfunctions.R
+++ R/amerasfunctions.R
@@ -2078,7 +2078,7 @@ ameras.fma <- function(family, dosevars, data, deg, transform=NULL,transform.jac
 }
 
 boundedSliceSampler <- nimbleFunction(
-  contains = sampler_BASE,
+  contains = nimble::sampler_BASE,
   setup = function(model, mvSaved, target, control) {
     # Create internal slice sampler safely
     sliceSampler <- nimble::sampler_slice(
@@ -4086,4 +4086,31 @@ traceplot.amerasfit <- function(object, iter=5000,  Rhat=TRUE, n.eff=TRUE, pdf=F
   }
   
 }
+
+# ecdfplot <- function(data, dosevars, xlab="Dose", ylab="Cumulative distribution"){
+#   
+#   check_pkgs(c("dplyr","ggplot2", "tidyr"))
+#   
+#   dose <- curve_id <- row_id <- NULL # To suppress R CMD CHECK notes about undefined global variables
+#   
+#   dosemat <- data[, dosevars, drop=FALSE ]
+#   
+#   dosemat |> 
+#     dplyr::mutate(row_id = dplyr::row_number()) |> 
+#     tidyr::pivot_longer(
+#       cols = -row_id,
+#       names_to = "curve_id",
+#       values_to = "dose"
+#     ) |> 
+#     ggplot2::ggplot() +
+#     ggplot2::stat_ecdf(
+#       ggplot2::aes(x = dose, group = curve_id),
+#       color = "grey",
+#       alpha = 0.2
+#     ) +
+#     ggplot2::theme_minimal(base_size = 15) +
+#     ggplot2::xlab(xlab) +
+#     ggplot2::ylab(ylab) +
+#     ggplot2::scale_x_log10(labels = scales::comma)
+# }
 
